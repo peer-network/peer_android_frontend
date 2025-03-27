1ee3982f27259ce35e7d08280fe3d33ffd324d0b
Sandro Megrelishvili <sandro.bandro10@gmail.com>
2025-03-27 16:35:51 +0100

    [86c2rzpfj]: Complete Video Component

--- core/ui/src/main/kotlin/eu/peernetwork/core/ui/compose/DesignOption.kt
+++ core/ui/src/main/kotlin/eu/peernetwork/core/ui/compose/DesignOption.kt
@@ -1,9 +1,14 @@
 package eu.peernetwork.core.ui.compose
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +22,11 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
+enum class DesignOptionPosition {
+    START,
+    BOTTOM
+}
+
 @Composable
 fun DesignOption(
     text: String,
@@ -27,18 +37,42 @@ fun DesignOption(
     contentPaddingValues: PaddingValues = PaddingValues(horizontal = 6.dp),
     tint: Color = LocalContentColor.current,
     textStyle: TextStyle = MaterialTheme.typography.bodySmall,
+    position: DesignOptionPosition = DesignOptionPosition.START
 ) {
-    Row(
-        modifier = modifier
-            .clickable(role = Role.Button, onClick = onClick)
-            .padding(contentPaddingValues),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            painter = painter,
-            tint = tint,
-            contentDescription = contentDescription
-        )
-        Text(text, style = textStyle, modifier = Modifier.padding(end = 4.dp))
+    when (position) {
+        DesignOptionPosition.START -> {
+            Row(
+                modifier = modifier
+                    .clickable(role = Role.Button, onClick = onClick)
+                    .padding(contentPaddingValues),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painter,
+                    tint = tint,
+                    contentDescription = contentDescription,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text, style = textStyle)
+            }
+        }
+        DesignOptionPosition.BOTTOM -> {
+            Column(
+                modifier = modifier
+                    .clickable(role = Role.Button, onClick = onClick)
+                    .padding(contentPaddingValues),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painter,
+                    tint = tint,
+                    contentDescription = contentDescription,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text, style = textStyle)
+            }
+        }
     }
 }
