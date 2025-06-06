52e7cea09b4a0ba6a6e9d52ce5a71adf508ba17b
conxtantyn <conxtantyn@gmail.com>
2025-06-06 16:47:39 +0200

    chore(home): fix title bar alignment

--- app/src/main/kotlin/eu/peernetwork/app/ui/feed/FeedScreen.kt
+++ app/src/main/kotlin/eu/peernetwork/app/ui/feed/FeedScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -138,7 +139,11 @@ fun FeedScreen(
                         .clickable(
                             role = Role.Button,
                             onClick = {  })) {
-                        Text(title ?: stringResource(R.string.feed_label))
+                        Text(
+                            title ?: stringResource(R.string.feed_label),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                 }
             }
