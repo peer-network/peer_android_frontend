436c148957e45ca00b3d80daa6eb980c889cfd50
ramesh <rameshgetit@gmail.com>
2025-06-26 13:41:09 +0200

    git commit -m "Fullscreen Post Viewer with Swipe Navigation" - removed unwanted FollowButton.kt and removed the white color in PostIcon.kt

--- blog/ui/src/main/kotlin/eu/peernetwork/blog/ui/compose/PostIcon.kt
+++ blog/ui/src/main/kotlin/eu/peernetwork/blog/ui/compose/PostIcon.kt
@@ -23,7 +23,7 @@ fun PostIcon(
     action: UiAction,
     value: String,
     isHorizontal: Boolean = true,
-    color: Color = Color.White,
+    color: Color = MaterialTheme.colorScheme.tertiary,
     onClick: (UiAction) -> Unit,
 ) {
     val clickHandler = remember(action) { { onClick(action) } }
