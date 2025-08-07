3768234fc25c26dcdbd00ece9ef724fdb75479bd
SandrZeus <sandro.bandro10@gmail.com>
2025-08-07 11:22:25 +0200

    just coming soon

--- messaging/ui/src/main/kotlin/eu/peernetwork/messaging/ui/chat/ChatScreen.kt
+++ messaging/ui/src/main/kotlin/eu/peernetwork/messaging/ui/chat/ChatScreen.kt
@@ -1,35 +1,26 @@
 package eu.peernetwork.messaging.ui.chat
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.ViewModelStoreOwner
-import eu.peernetwork.core.ui.R
-import eu.peernetwork.core.ui.component.UiComponentProvider
-import eu.peernetwork.core.ui.design.compose.DesignTitle
-import eu.peernetwork.core.ui.design.compose.DesignTitleBarHost
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun ChatScreen(
-    show: Boolean,
-    provider: UiComponentProvider,
-    viewModelStoreOwner: ViewModelStoreOwner
-) {
-    Column(modifier = Modifier.fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
-        if (show) {
-            DesignTitleBarHost("ChatScreen") {
-                titleBar {
-                    DesignTitle {
-                        Text(stringResource(R.string.chat_label))
-                    }
-                }
-            }
-        }
+fun ChatScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Coming soon",
+            style = MaterialTheme.typography.headlineLarge.copy(MaterialTheme.colorScheme.onBackground),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
