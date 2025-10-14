2e45604643f9d6c72042516887411ae64faf0fd1
Novachrono-Ryhze <srivetrikumaransv@gmail.com>
2025-10-14 14:15:35 +0200

    comment bug fix

--- blog/ui/src/main/kotlin/eu/peernetwork/blog/ui/comment/CommentScreen.kt
+++ blog/ui/src/main/kotlin/eu/peernetwork/blog/ui/comment/CommentScreen.kt
@@ -4,9 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.input.TextFieldState
@@ -197,12 +195,10 @@ fun CommentScreen(
                 viewModel.clear()
             }
         }
-        LaunchedEffect(state.value) {
+        LaunchedEffect(isCommentPosted.value) {
             if (state.value != null) {
                 state.value?.let {
-                    if (it.id != (viewModel.state.value as? CommentViewModel.State.Success)?.postId) {
                         viewModel.load(it.id, Pageable(0, postLimit))
-                    }
                 }
             } else {
                 viewModel.reset()
