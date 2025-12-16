4d0ddf56a7202944126a0b5518c0539b66aada28
conxtantyn <conxtantyn@gmail.com>
2025-12-16 13:08:43 +0100

    chore(user): fix login and deactivation sheet style

--- user/ui/src/main/kotlin/eu/peernetwork/user/ui/deactivate/DeactivateScreen.kt
+++ user/ui/src/main/kotlin/eu/peernetwork/user/ui/deactivate/DeactivateScreen.kt
@@ -1,43 +1,21 @@
 package eu.peernetwork.user.ui.deactivate
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.text.input.TextObfuscationMode
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import eu.peernetwork.core.ui.component.UiComponentProvider
 import eu.peernetwork.core.ui.design.material.DesignBottomSheetScaffold
-import eu.peernetwork.core.ui.design.material.DesignButton
-import eu.peernetwork.core.ui.design.material.DesignSecureTextField
 import eu.peernetwork.core.ui.extension.builder
-import eu.peernetwork.core.ui.extension.isValidInput
-import eu.peernetwork.user.ui.R
 
 @Composable
 fun DeactivateScreen(
@@ -61,16 +39,16 @@ fun DeactivateScreen(
     val focus = remember { FocusRequester() }
     DesignBottomSheetScaffold(
         state = show,
-        color = MaterialTheme.colorScheme.surfaceContainerLow,
+        color = MaterialTheme.colorScheme.surfaceDim,
         onShow = { focus.requestFocus() },
         onDismiss = { show.value = false }
     ) {
-        DeactivateScreen(
+        DeactivateSheet(
             state = show,
             isLoading = isLoading,
             password = password,
-            label =  stringResource(R.string.deactivate_text),
-            focus = focus
+            focus = focus,
+            onCancel = { show.value = false }
         ) { viewModel(password.text.toString()) }
     }
     LaunchedEffect(isSuccess.value) {
@@ -78,52 +56,5 @@ fun DeactivateScreen(
             show.value = false
             viewModel.reset()
         }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
-fun DeactivateScreen(
-    state: MutableState<Boolean>,
-    isLoading: State<Boolean>,
-    password: TextFieldState,
-    label: String,
-    focus: FocusRequester,
-    onSubmit: (String) -> Unit = {}
-) {
-    val isValidated = remember { derivedStateOf {
-        state.value && password.isValidInput() && !isLoading.value
-    } }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp)
-    ) {
-        DesignSecureTextField(
-            state = password,
-            focusRequester = focus,
-            enabled = state.value,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            ),
-            placeholder = { Text(stringResource(id = R.string.password_label)) },
-            textObfuscationMode = TextObfuscationMode.Hidden,
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        DesignButton(
-            enabled = isValidated.value,
-            isLoading = isLoading.value,
-            onClick = { onSubmit(password.text.toString()) },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = label,
-                style = MaterialTheme.typography.bodyMedium.copy(
-                    fontWeight = FontWeight.Bold
-                )
-            )
-        }
-        Spacer(modifier = Modifier.navigationBarsPadding())
     }
 }
--- user/ui/src/main/kotlin/eu/peernetwork/user/ui/deactivate/DeactivateSheet.kt
+++ user/ui/src/main/kotlin/eu/peernetwork/user/ui/deactivate/DeactivateSheet.kt
@@ -1,4 +1,4 @@
-package eu.peernetwork.user.ui.logout
+package eu.peernetwork.user.ui.deactivate
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,66 +7,50 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.TextObfuscationMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.peernetwork.core.ui.design.luna.DesignButton
 import eu.peernetwork.core.ui.design.luna.DesignOutlineButton
+import eu.peernetwork.core.ui.design.luna.DesignPassword
 import eu.peernetwork.core.ui.design.luna.designSecondaryButtonColors
-import eu.peernetwork.core.ui.design.material.DesignBottomSheetScaffold
+import eu.peernetwork.core.ui.extension.isValidInput
 import eu.peernetwork.core.ui.theme.DesignTheme
 import eu.peernetwork.user.ui.R
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun LogoutSheet(
-    state: MutableState<Boolean>,
-    isLoading: State<Boolean>,
-    onLogout: () -> Unit = {}
-) {
-    val handleLogout by rememberUpdatedState(onLogout)
-    val action = remember { mutableStateOf<(() -> Unit)?>(null) }
-    DesignBottomSheetScaffold(
-        state = state,
-        color = MaterialTheme.colorScheme.surfaceDim,
-        onDismiss = {
-            action.value?.invoke()
-            action.value = null
-            state.value = false
-        }
-    ) {
-        LogoutSheet(
-            isLoading = isLoading,
-            onCancel = { state.value = false },
-            modifier = Modifier
-        ) {
-            action.value = handleLogout
-            state.value = false
-        }
-    }
-}
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun LogoutSheet(
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+fun DeactivateSheet(
+    state: MutableState<Boolean>,
     isLoading: State<Boolean>,
-    modifier: Modifier = Modifier,
-    onCancel: () -> Unit,
-    onLogout: () -> Unit,
+    password: TextFieldState,
+    focus: FocusRequester,
+    onCancel: () -> Unit = {},
+    onSubmit: (String) -> Unit = {}
 ) {
+    val isValidated = remember { derivedStateOf {
+        state.value && password.isValidInput() && !isLoading.value
+    } }
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -74,20 +58,26 @@ fun LogoutSheet(
             .padding(top = 16.dp)
             .padding(bottom = 12.dp)
             .navigationBarsPadding()
-            .then(modifier)
     ) {
-        LogoutTitle()
-        Text(
-            stringResource(R.string.logout_message),
-            style = MaterialTheme.typography.labelLarge,
-            color = MaterialTheme.colorScheme.outline,
+        DeactivateTitle()
+        DesignPassword(
+            state = password,
+            hint = stringResource(id = R.string.password_label),
+            focusRequester = focus,
+            enabled = state.value,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            contentPadding = PaddingValues(16.dp),
+            textObfuscationMode = TextObfuscationMode.Hidden,
             modifier = Modifier.fillMaxWidth()
-                .padding(top = 12.dp)
+                .padding(top = 16.dp)
         )
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(12.dp),
-            modifier = Modifier.padding(top = 16.dp)
+            modifier = Modifier.padding(top = 12.dp)
                 .padding(bottom = 6.dp)
         ) {
             DesignOutlineButton(
@@ -100,16 +90,17 @@ fun LogoutSheet(
                 modifier = Modifier.weight(1f)
             ) { Text(stringResource(R.string.cancel_label)) }
             DesignButton(
-                onClick = onLogout,
+                onClick = { onSubmit(password.text.toString()) },
                 minHeight = 42.dp,
-                enabled = !isLoading.value,
+                enabled = isValidated.value,
+                isLoading = isLoading.value,
                 style = MaterialTheme.typography.bodyMedium.copy(
                     fontWeight = FontWeight.SemiBold
                 ),
                 colors = designSecondaryButtonColors(),
                 contentPadding = PaddingValues(horizontal = 12.dp),
                 modifier = Modifier.weight(1f)
-            ) { Text(stringResource(R.string.logout_text)) }
+            ) { Text(stringResource(R.string.deactivate_text)) }
         }
     }
 }
@@ -119,10 +110,15 @@ fun LogoutSheet(
 @OptIn(ExperimentalMaterial3Api::class)
 fun PreviewLogoutSheet() {
     DesignTheme(isDarkMode = true) {
+        val state = remember { mutableStateOf(false) }
         val isLoading = remember { mutableStateOf(false) }
-        LogoutSheet(
+        val password = remember { TextFieldState() }
+        val focus = remember { FocusRequester() }
+        DeactivateSheet(
+            state = state,
             isLoading = isLoading,
-            onCancel = {}
+            password = password,
+            focus = focus
         ) {}
     }
 }
